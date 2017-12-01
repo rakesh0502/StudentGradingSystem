01d889c9d69ee51127231a22b525a4b1f74116e0
Rakesh <Rakesh@Rakesh-PC>
2017-12-01 16:22:04 -0500

    changes in main

--- src/com/ub/sec/studentgrader/client/Main.java
+++ src/com/ub/sec/studentgrader/client/Main.java
@@ -15,19 +15,27 @@ public class Main {
 		l1.setBounds(20, 20, 140, 30);
 		JButton b = new JButton("Submit");
 		b.setBounds(50, 80, 100, 30);
-		final JTextField text = new JTextField();
-		text.setBounds(150, 20, 50, 30);
+		
+		Integer[] members = new Integer[] {2, 3, 4, 5, 6, 7};
+		 
+		JComboBox<Integer> memberOptions = new JComboBox<Integer>(members);
+		memberOptions.setBounds(150, 20, 50, 30);
+		//final JTextField text = new JTextField();
+		//text.setBounds(150, 20, 50, 30);
 		f.add(l1);
 		f.add(label);
 		f.add(b);
-		f.add(text);
+		f.add(memberOptions);
 		f.setSize(300, 300);
 		f.setLayout(null);
 		f.setVisible(true);
-		b.addActionListener(new ActionListener() {
+		memberOptions.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String data = "Members " + text.getText();
-				label.setText(data);
+				//String data = "Members " + text.getText();
+				JComboBox<Integer> memberOptions = (JComboBox<Integer>)e.getSource();
+				int n = (Integer) memberOptions.getSelectedItem();
+				System.out.println("Number entered: "+n);
+				label.setText("Number entered: "+n);
 			}
 		});
 	}
