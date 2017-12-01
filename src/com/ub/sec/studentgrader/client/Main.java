707bc7fc6240dbdde34102418ee9413c52416201
sourabh <sourabhb@buffalo.edu>
2017-12-01 16:51:48 -0500

    Added normalized method

--- src/com/ub/sec/studentgrader/client/Main.java
+++ src/com/ub/sec/studentgrader/client/Main.java
@@ -7,10 +7,7 @@ import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-		
-=======
+
 		JFrame f = new JFrame("Student Grading system");
 		final JLabel label = new JLabel();
 		label.setBounds(100, 150, 250, 50);
@@ -34,6 +31,80 @@ public class Main {
 				label.setText(data);
 			}
 		});
->>>>>>> fa69103c9b975794a3131af0bd48bd1f7564b16c
+		
+		int[] professionalism  = {5,4,5,1,2};
+		int[] meetingParticipation  = {5,4,5,1,2};
+		int[] workEvaluation  = {5,4,5,1,2};
+		
+		double[] result = doNormalization(professionalism, meetingParticipation, workEvaluation, 5);
+		printArray(result);
 	}
+	
+	 /*doNormalization - Does the normalization of the scores
+	 * Input : 3 scores array, total students
+	 * Output : Option 1 : //boolean flag which is true if Normalization is done successfully, else false
+	 * 		  : Option 2 : Return normalizedScore Array, if null returned then error in the method
+	 * Reference : http://home.apu.edu/~bsimmerok/WebTMIPs/Session7/TSes7.html
+	 * 
+	 * Example : 
+	 * Number of students : 5
+	 * professionalism      = {5,4,5,1,2}
+	 * meetingParticipation = {5,4,5,1,2}
+	 * workEvaluation       = {5,4,5,1,2}
+	 * 
+	 * Algorithm : 
+	 * 1. Calculate maximum score for a student, index represents the student
+	 * 2. Normalized score is calculated by  professionalism[i] + meetingParticipation[i] + workEvaluation[i]/ maxScore
+	 * 2. return Normalized score array
+	 * 
+	 * Test Cases : 
+	 * 1. Non equal length of 3 scores array
+	 * 2. Negative values in any of 3 scores array
+	 * 3. normalizedScore array should contain valid scores //TODO
+	 * 4. Everyone gets Zero //TODO
+	 * 
+	 * */
+	static double[] doNormalization(int[] professionalism,int[] meetingParticipation,int[] workEvaluation, int totalStudents) {
+		
+		//normalizedScore array
+		double[] normalizedScore = new double[totalStudents];
+		int maxScore = 0;
+		  
+		//TestCase #1 : check of all evaluations array have same lengths
+		if(professionalism.length != totalStudents && meetingParticipation.length != totalStudents && workEvaluation.length != totalStudents) {
+			return null;
+		}
+		
+		//TestCase #2 : Negative values in any of 3 scores array
+		for(int i=0;i<totalStudents;i++) {
+			if(professionalism[i] < 0 || meetingParticipation[i] < 0 || workEvaluation[i] < 0) {
+				return null;
+			}
+		}
+		
+		//step 1 : Calculate maximum score for a student, index represents the student
+		for(int i=0;i<totalStudents;i++) {
+			if (professionalism[i] + meetingParticipation[i] + workEvaluation[i] > maxScore) {
+				maxScore = professionalism[i] + meetingParticipation[i] + workEvaluation[i];
+			}
+		}
+		
+		System.out.print("MaxScore = "+ maxScore + "\n");
+		
+		//step 2: do normalization for each student and assign it to normalizedScore array
+		for(int i=0;i<totalStudents;i++) {
+			System.out.println(professionalism[i] + meetingParticipation[i] + workEvaluation[i] + " " + i + "\n");
+			normalizedScore[i] = professionalism[i] + meetingParticipation[i] + workEvaluation[i]  / maxScore * 10;
+		}
+		return normalizedScore;
+	}
+	
+	//helper function to print Array
+	public static void printArray(double[] input) {
+		for(int i=0;i<input.length;i++) {
+			System.out.print(input[i] + " ") ;
+		}
+			
+	}
+	
 }
